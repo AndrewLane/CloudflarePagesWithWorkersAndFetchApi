f8d4003371ae6003c78695cebee9acefddd9c27c
Andrew Lane <andrew.lane@powerschool.com>
2022-01-23 23:30:22 -0500

    Try to get SWAPI to work

--- functions/use-fetch.js
+++ functions/use-fetch.js
@@ -1,6 +1,10 @@
 export async function onRequestGet({}) {
-  const res = await fetch(`https://rickandmortyapi.com/api/character/12`);
-  const data = await res.json();
+  const url = "https://swapi.dev/api/planets/1/";
+  const result = await fetch(url, {
+    method: "GET",
+    cache: "no-cache",
+  });
+  const data = await result.json();
   const info = JSON.stringify(data);
   return new Response(info, null, 2);
 }
