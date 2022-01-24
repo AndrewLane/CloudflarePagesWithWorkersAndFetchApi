2e674f8f5ded39001a12566617ffafd0d0a05beb
Andrew Lane <andrew.lane@powerschool.com>
2022-01-23 23:19:11 -0500

    Try different syntax

--- functions/use-fetch.js
+++ functions/use-fetch.js
@@ -1,7 +1,9 @@
 export const onRequestGet = async ({}) => {
   const url = "https://swapi.dev/api/planets/1/";
-  return fetch(url, {
+  const result = await fetch(url, {
     method: "GET",
     cache: "no-cache",
   });
+  const data = await result.json();
+  return new Response(JSON.stringify(data));
 };
