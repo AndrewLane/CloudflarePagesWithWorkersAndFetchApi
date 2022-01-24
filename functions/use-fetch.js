04a2e3ad3942ea7f4c88e83ee64b3a23421ce089
Andrew Lane <andrew.lane@powerschool.com>
2022-01-23 23:41:03 -0500

    Go back to the original syntax and just take out the cache

--- functions/use-fetch.js
+++ functions/use-fetch.js
@@ -1,9 +1,6 @@
-export async function onRequestGet({}) {
-  const url = "https://swapi.dev/api/planets/1/";
-  const result = await fetch(url, {
+export const onRequestGet = async ({}) => {
+  const url = "https://swapi.dev/api/planets/2/";
+  return fetch(url, {
     method: "GET",
   });
-  const data = await result.json();
-  const info = JSON.stringify(data);
-  return new Response(info, null, 2);
-}
+};
